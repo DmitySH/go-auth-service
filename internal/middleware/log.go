75b10ea45c4f662f97424d0721e84144511273dd
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2023-05-03 17:59:42 +0300

    add password hiding in logs

--- internal/middleware/log.go
+++ internal/middleware/log.go
@@ -2,16 +2,28 @@ package middleware
 
 import (
 	"context"
+	"fmt"
 	"github.com/DmitySH/go-auth-service/pkg/log"
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
+	"regexp"
 )
 
 func LogInterceptor(ctx context.Context,
 	req interface{},
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler) (interface{}, error) {
+	reqUUID := uuid.New()
 
-	log.Logger().Infof("called %s method | parameters: {%v}", info.FullMethod, req)
+	logStr := fmt.Sprintf("Request: %s | Method: %s | parameters: {%+v}", reqUUID, info.FullMethod, req)
+	safeLogStr := hidePasswordFromLog(logStr)
+	log.Logger().Infof(safeLogStr)
 
 	return handler(ctx, req)
+}
+
+func hidePasswordFromLog(logStr string) string {
+	re := regexp.MustCompile(`password:"(.*)"`)
+
+	return re.ReplaceAllString(logStr, `password:"***"`)
 }
